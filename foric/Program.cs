f52431bf8c22e448eca8e9ee6fab23677c411328
Guven Servis <Guven Servis@DESKTOP-1QRA49N>
2019-11-07 03:53:09 +0400

    Add project files.

---
+++ foric/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] heyvan_isimleri = new string[5] { "ayi", "cavid", "meymun", "dovsan", "bal porsugu" };
+            Array.Clear(heyvan_isimleri, 2, 2);
+
+            foreach (string adlar in heyvan_isimleri) 
+            {
+                Console.WriteLine(adlar);
+
+            }Console.ReadKey();
+        }
+    }
+}
